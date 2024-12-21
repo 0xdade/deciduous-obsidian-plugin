893217d13eb489722110c891381fddb7ddbf8593
0xdade <0xdade@users.noreply.github.com>
2024-12-21 11:07:59 -0800

    Fix readme, format code consistently

--- main.ts
+++ main.ts
@@ -11,35 +11,35 @@ export default class Deciduous extends Plugin {
 		source: string,
 		el: HTMLElement,
 		ctx: MarkdownPostProcessorContext
-	  ) {
+	) {
 		try {
-		  const container = document.createElement("div");
-		  container.style.width = "100%";
-		  container.style.height = "auto";
-		  el.appendChild(container);
-		  const parsed = jsyaml.load(source)
+			const container = document.createElement("div");
+			container.style.width = "100%";
+			container.style.height = "auto";
+			el.appendChild(container);
+			const parsed = jsyaml.load(source)
 
-		  const { dot, types, title } = convertToDot(parsed);
-		  const currentSVG = this.graphviz.layout(dot, "svg", "dot");
-		  const svgElement = new DOMParser().parseFromString(currentSVG, "image/svg+xml").documentElement;
-		  svgElement.setAttribute("preserveAspectRatio", "xMidYMid meet");
-		  svgElement.setAttribute("width", "100%");
-		  svgElement.setAttribute("height", "100%");
-		  container.appendChild(svgElement)
+			const { dot, types, title } = convertToDot(parsed);
+			const currentSVG = this.graphviz.layout(dot, "svg", "dot");
+			const svgElement = new DOMParser().parseFromString(currentSVG, "image/svg+xml").documentElement;
+			svgElement.setAttribute("preserveAspectRatio", "xMidYMid meet");
+			svgElement.setAttribute("width", "100%");
+			svgElement.setAttribute("height", "100%");
+			container.appendChild(svgElement)
 		} catch (error) {
-		  console.error("Error rendering Deciduous diagram:", error);
-		  const errorMessage = document.createElement("pre");
-		  errorMessage.textContent = `Error: ${error.message}`;
-		  el.appendChild(errorMessage);
+			console.error("Error rendering Deciduous diagram:", error);
+			const errorMessage = document.createElement("pre");
+			errorMessage.textContent = `Error: ${error.message}`;
+			el.appendChild(errorMessage);
 		}
-	  }
+	}
 
 	async onload() {
 		this.graphviz = await Graphviz.load();
 		this.registerMarkdownCodeBlockProcessor(
 			"deciduous",
 			this.renderDeciduousDiagram.bind(this)
-			);
+		);
 	}
 
 	onunload() {
